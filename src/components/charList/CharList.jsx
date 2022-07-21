06cd39de612c6ac574d400b0fc22dd1f7a1a43e9
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-07-22 00:04:59 +0300

    feat: Make animation when list items appear

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,121 +1,120 @@
+//@Libs
 import { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { scroller } from 'react-scroll';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+//@Components
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../error/ErrorMessage';
-
+//@Styles
 import './charList.scss';
 
 const CharList = (props) => {
-    const [charactersList, setCharactersList] = useState([]);
-    const [offset, setOffset] = useState(210);
+	const [charactersList, setCharactersList] = useState([]);
+	const [offset, setOffset] = useState(210);
 
-    const { loading, error, getAllCharacters } = useMarvelService();
+	const { loading, error, getAllCharacters } = useMarvelService();
 
-    useEffect(() => {
-        updateCharacters();
-        // eslint-disable-next-line
-    }, []);
+	useEffect(() => {
+		updateCharacters();
+		// eslint-disable-next-line
+	}, []);
 
-    const updateCharacters = (offset) => {
-        getAllCharacters(offset).then(onCharLoaded);
-    };
+	const updateCharacters = (offset) => {
+		getAllCharacters(offset).then(onCharLoaded);
+	};
 
-    const onCharLoaded = (newCharacters) => {
-        setCharactersList((charactersList) => [
-            ...charactersList,
-            ...newCharacters,
-        ]);
-        setOffset((offset) => offset + 9);
-    };
+	const onCharLoaded = (newCharacters) => {
+		setCharactersList((charactersList) => [
+			...charactersList,
+			...newCharacters,
+		]);
+		setOffset((offset) => offset + 9);
+	};
 
-    const itemsRef = useRef([]);
+	const itemsRef = useRef([]);
 
-    const focusOnItem = (i) => {
-        itemsRef.current.forEach((item) => {
-            item.classList.remove('char__item_selected');
-        });
+	const focusOnItem = (i) => {
+		itemsRef.current.forEach((item) => {
+			item.classList.remove('char__item_selected');
+		});
 
-        itemsRef.current[i].classList.add('char__item_selected');
-        itemsRef.current[i].focus();
-    };
+		itemsRef.current[i].classList.add('char__item_selected');
+		itemsRef.current[i].focus();
+	};
 
-    const scrollTo = () => {
-        scroller.scrollTo('char__info', {
-            duration: 900,
-            delay: 100,
-            smooth: true,
-            offset: -20,
-        });
-    };
+	const scrollTo = () => {
+		scroller.scrollTo('char__info', {
+			duration: 900,
+			delay: 100,
+			smooth: true,
+			offset: -20,
+		});
+	};
 
-    function renderCharactersList(arrayOfCharacters) {
-        const charactersListItems = arrayOfCharacters.map((char, i) => {
-            let imgStyle = { objectFit: 'cover' };
+	function renderCharactersList(arrayOfCharacters) {
+		const charactersListItems = arrayOfCharacters.map((char, i) => {
+			let imgStyle = { objectFit: 'cover' };
 
-            if (
-                char.thumbnail ===
-                'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
-            ) {
-                imgStyle = { objectFit: 'fill' };
-            }
-            return (
-                <li
-                    ref={(el) => (itemsRef.current[i] = el)}
-                    key={char.id}
-                    tabIndex={0}
-                    className='char__item'
-                    onClick={() => {
-                        props.onCharSelected(char.id);
-                        focusOnItem(i);
-                        scrollTo();
-                    }}
-                    onKeyPress={(e) => {
-                        if (e.key === ' ' || e.key === 'Enter') {
-                            e.preventDefault();
-                            props.onCharSelected(char.id);
-                            focusOnItem(i);
-                            scrollTo();
-                        }
-                    }}
-                >
-                    <img
-                        src={char.thumbnail}
-                        alt={char.name}
-                        style={imgStyle}
-                    />
-                    <div className='char__name'>{char.name}</div>
-                </li>
-            );
-        });
+			if (
+				char.thumbnail ===
+				'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+			) {
+				imgStyle = { objectFit: 'fill' };
+			}
+			return (
+				<CSSTransition key={char.id} timeout={1000} classNames='item' in={true}>
+					<li
+						ref={(el) => (itemsRef.current[i] = el)}
+						tabIndex={0}
+						className='char__item'
+						onClick={() => {
+							props.onCharSelected(char.id);
+							focusOnItem(i);
+							scrollTo();
+						}}
+						onKeyPress={(e) => {
+							if (e.key === ' ' || e.key === 'Enter') {
+								e.preventDefault();
+								props.onCharSelected(char.id);
+								focusOnItem(i);
+								scrollTo();
+							}
+						}}
+					>
+						<img src={char.thumbnail} alt={char.name} style={imgStyle} />
+						<div className='char__name'>{char.name}</div>
+					</li>
+				</CSSTransition>
+			);
+		});
 
-        return <ul className='char__grid'>{charactersListItems}</ul>;
-    }
+		return <ul className='char__grid'>{charactersListItems}</ul>;
+	}
 
-    const listOfCharacters = renderCharactersList(charactersList);
+	const listOfCharacters = renderCharactersList(charactersList);
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
 
-    return (
-        <div className='char__list'>
-            {listOfCharacters}
-            {errorMessage}
-            {spinner}
-            <button
-                className='button button_main button_long'
-                onClick={() => updateCharacters(offset)}
-            >
-                <div className='inner'>load more</div>
-            </button>
-        </div>
-    );
+	return (
+		<div className='char__list'>
+			<TransitionGroup component={null}>{listOfCharacters}</TransitionGroup>
+			{errorMessage}
+			{spinner}
+			<button
+				className='button button_main button_long'
+				onClick={() => updateCharacters(offset)}
+			>
+				<div className='inner'>load more</div>
+			</button>
+		</div>
+	);
 };
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func,
+	onCharSelected: PropTypes.func,
 };
 
 export default CharList;
--- src/components/comicsList/ComicsList.jsx
+++ src/components/comicsList/ComicsList.jsx
@@ -1,75 +1,73 @@
+//@Libs
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+//@Components
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../error/ErrorMessage';
-
+//@Styles
 import './comicsList.scss';
 
 const ComicsList = () => {
-    const { loading, error, clearError, getComicses } = useMarvelService();
-    const [comicses, setComicses] = useState([]);
-    const [offset, setOffset] = useState(210);
+	const { loading, error, clearError, getComicses } = useMarvelService();
+	const [comicses, setComicses] = useState([]);
+	const [offset, setOffset] = useState(210);
 
-    useEffect(() => {
-        updateComicses();
-        // eslint-disable-next-line
-    }, []);
+	useEffect(() => {
+		updateComicses();
+		// eslint-disable-next-line
+	}, []);
 
-    const updateComicses = (offset) => {
-        clearError();
-        getComicses(offset).then(onComicsesLoaded);
-    };
+	const updateComicses = (offset) => {
+		clearError();
+		getComicses(offset).then(onComicsesLoaded);
+	};
 
-    const onComicsesLoaded = (newComicses) => {
-        setComicses((comicsesList) => [...comicsesList, ...newComicses]);
-        setOffset((offset) => offset + 8);
-    };
+	const onComicsesLoaded = (newComicses) => {
+		setComicses((comicsesList) => [...comicsesList, ...newComicses]);
+		setOffset((offset) => offset + 8);
+	};
 
-    const renderComicsesList = (comicses) => {
-        const comicsesListItems = comicses.map(
-            (comic, i) => {
-                return (
-                    <li className='comics__item' key={i}>
-                        <Link to={`${comic.id}`}>
-                            <img
-                                src={comic.thumbnail}
-                                alt={comic.title}
-                                className='comics__item-img'
-                            />
-                            <div className='comics__item-name'>
-                                {comic.title}
-                            </div>
-                            <div className='comics__item-price'>
-                                {comic.price}
-                            </div>
-                        </Link>
-                    </li>
-                );
-            }
-        );
-        return <ul className='comics__grid'>{comicsesListItems}</ul>;
-    };
+	const renderComicsesList = (comicses) => {
+		const comicsesListItems = comicses.map((comic, i) => {
+			return (
+				<CSSTransition key={i} timeout={1000} classNames='item' in={true}>
+					<li className='comics__item' >
+					<Link to={`${comic.id}`}>
+						<img
+							src={comic.thumbnail}
+							alt={comic.title}
+							className='comics__item-img'
+						/>
+						<div className='comics__item-name'>{comic.title}</div>
+						<div className='comics__item-price'>{comic.price}</div>
+					</Link>
+				</li>
+				</CSSTransition>
+			);
+		});
+		return <ul className='comics__grid'>{comicsesListItems}</ul>;
+	};
 
-    const listOfComicses = renderComicsesList(comicses);
+	const listOfComicses = renderComicsesList(comicses);
 
-    const spinner = loading ? <Spinner /> : null;
-    const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const errorMessage = error ? <ErrorMessage /> : null;
 
-    return (
-        <div className='comics__list'>
-            {listOfComicses}
-            {errorMessage}
-            {spinner}
-            <button
-                onClick={() => updateComicses(offset)}
-                className='button button_main button_long'
-            >
-                <div className='inner'>load more</div>
-            </button>
-        </div>
-    );
+	return (
+		<div className='comics__list'>
+			<TransitionGroup component={null}>{listOfComicses}</TransitionGroup>
+			{errorMessage}
+			{spinner}
+			<button
+				onClick={() => updateComicses(offset)}
+				className='button button_main button_long'
+			>
+				<div className='inner'>load more</div>
+			</button>
+		</div>
+	);
 };
 
 export default ComicsList;
