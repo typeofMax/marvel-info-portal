1e5634c15f8d257903d298e447c1168759c7ba6a
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-07-21 23:19:19 +0300

    style: Change tab size

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,30 +4,24 @@ import AppHeader from '../appHeader/AppHeader';
 import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <div className='app'>
-                <AppHeader />
-                <main>
-                    <Routes>
-                        <Route
-                            path='/marvel-info-portal'
-                            element={<MainPage />}
-                        />
-                        <Route
-                            path='/marvel-info-portal/comics'
-                            element={<ComicsPage />}
-                        />
-                        <Route
-                            path='/marvel-info-portal/comics/:comicId'
-                            element={<SingleComicPage />}
-                        />
-                        <Route path='*' element={<Page404 />} />
-                    </Routes>
-                </main>
-            </div>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<div className='app'>
+				<AppHeader />
+				<main>
+					<Routes>
+						<Route path='/marvel-info-portal' element={<MainPage />} />
+						<Route path='/marvel-info-portal/comics' element={<ComicsPage />} />
+						<Route
+							path='/marvel-info-portal/comics/:comicId'
+							element={<SingleComicPage />}
+						/>
+						<Route path='*' element={<Page404 />} />
+					</Routes>
+				</main>
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
